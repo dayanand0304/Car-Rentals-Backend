833056e936bbf7d0d15255353450f54a6698db7c
dayanand0304 <dayanand0033@gmail.com>
2026-01-26 15:46:00 +0530

    feat: added Global Exception Handler

---
+++ src/main/java/com/CarRentalSystem/CarRentals/ExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,85 @@
+package com.CarRentalSystem.CarRentals.ExceptionHandler;
+
+import com.CarRentalSystem.CarRentals.CustomExceptions.Cars.CarNotAvailableException;
+import com.CarRentalSystem.CarRentals.CustomExceptions.Cars.CarNotFoundException;
+import com.CarRentalSystem.CarRentals.CustomExceptions.Customers.CustomerNotFoundException;
+import com.CarRentalSystem.CarRentals.CustomExceptions.Rentals.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //404: NOT FOUND
+    @ExceptionHandler({
+            CarNotFoundException.class,
+            CustomerNotFoundException.class,
+            RentalNotFoundException.class
+    })
+    public ResponseEntity<ErrorResponse> handleNotFound(RuntimeException ex,
+                                                        HttpServletRequest request){
+        return buildErrorResponse(
+                HttpStatus.NOT_FOUND,
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
+    @ExceptionHandler({
+            DurationException.class,
+            RentalTypeException.class
+    })
+    public ResponseEntity<ErrorResponse> handleBadRequest(RuntimeException ex,
+                                                          HttpServletRequest request){
+        return buildErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
+    @ExceptionHandler({
+            CarNotAvailableException.class,
+            AlreadyReturnedException.class,
+            CannotCancelException.class
+
+    })
+    public ResponseEntity<ErrorResponse> handleConflict(RuntimeException ex,
+                                                        HttpServletRequest request){
+        return buildErrorResponse(
+                HttpStatus.CONFLICT,
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        return buildErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Unexpected server error",
+                request.getRequestURI()
+        );
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status,
+                                                             String message,
+                                                             String path){
+        ErrorResponse error=new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                status.name(),
+                message,
+                path
+        );
+        return ResponseEntity.status(status).body(error);
+    }
+}
