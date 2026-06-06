580df820089eae7bf6690573e17b8cf881b23ce9
dayanand0304 <dayanand0033@gmail.com>
2026-06-06 11:03:57 +0530

    chore(config): make local and test environments self-contained

--- src/test/java/com/CarRentalSystem/CarRentals/CarRentalsApplicationTests.java
+++ src/test/java/com/CarRentalSystem/CarRentals/CarRentalsApplicationTests.java
@@ -2,8 +2,10 @@ package com.CarRentalSystem.CarRentals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CarRentalsApplicationTests {
 
 	@Test
